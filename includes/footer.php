dfbb9bf72cdfdb9f40e3cf49a45c936fb7b0d0ac
liz20044 <116783809+liz20044@users.noreply.github.com>
2023-04-03 21:38:27 -0400

    Delete includes directory

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-           
-    </div>
-    <strong>©2022 Derechos Reservados. UPTA "Federico Brito Figueroa" <p style="float:right;color:black"href=""> UPTA  Aragua   <img width="22rem;" src="../images/logouni.jpg" alt=""></p></strong>
-</footer>
